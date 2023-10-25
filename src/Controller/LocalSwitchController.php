113d7ecf23243779d732e17afa87782056c2f119
Yamingue <yamking01@gmail.com>
2023-10-25 23:00:23 +0800

    finish trnslation

--- src/Controller/LocalSwitchController.php
+++ src/Controller/LocalSwitchController.php
@@ -3,10 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Translation\LocaleSwitcher;
 
 class LocalSwitchController extends AbstractController
 {
